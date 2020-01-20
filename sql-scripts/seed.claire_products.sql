d29231e9690f678cd4e16e17be4758fd905d1373
joel alva <panthersjoe@hotmail.com>
2020-01-20 00:27:27 -0500

    added sql

---
+++ sql-scripts/seed.claire_products.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+INSERT INTO claire_products
+    (product_name, image, product_descripton, price)
+VALUES
+('Platinum','image','12 gallons a minute','400$'),
+('Sliver','image','3 gallons a minute','50$'),
+('Clairify 1','image','1 gallon a minute','30$'),
+('Clairify RO','image','.5 gallon a minute','30$');
+
+COMMIT
+
+
+  
--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
+
+const knex = require('knex')
 const app = require('./app')
 const { PORT } = require('./config')
+
+
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+  })
+  
+  knexInstance.from('claire_products').select('*')
+    .then(result =>{
+        console.log(result)
+    })
+
+
+  
+
+
 app.listen (PORT,() =>{
     console.log('Server listening at http://localhost: ${PORT}')
 })
