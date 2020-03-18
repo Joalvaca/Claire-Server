9806563f2de782eb27ea3494f39ba5308aecb3af
joel Alva <panthersjoe@hotmail.com>
2020-03-18 17:40:23 -0400

    fixed for depoly

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://claire:1@localhost/products',
-  }
-  
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://claire:1@localhost/products",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || "postgresql://claire:1@localhost/products"
+};
